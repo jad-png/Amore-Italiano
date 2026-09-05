3be3fbb36416926e86928194b1441189b492743f
jad-png <j.belassiria3993@uca.ac.ma>
2026-09-05 19:50:08 +0100

    add clerk authentication page

--- app/admin/sign-in/[[...sign-in]]/page.tsx
+++ app/admin/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,11 @@ import { SignIn } from "@clerk/nextjs";
 export default function AdminSignInPage() {
   return (
     <main className="flex min-h-screen items-center justify-center px-6 py-32">
-      <SignIn path="/admin/sign-in" routing="path" fallbackRedirectUrl="/admin" />
+      <SignIn
+        path="/admin/sign-in"
+        routing="path"
+        fallbackRedirectUrl="/admin"
+      />
     </main>
   );
 }
