9e420f17df65d15835c1b2d91a641aca3cf650d7
jad-png <j.belassiria3993@uca.ac.ma>
2026-09-06 13:32:53 +0100

    Update admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,1 +1,8 @@
-export { default, dynamic, metadata } from "./dashboard/page";
+import DashboardPage, { metadata } from "./dashboard/page";
+
+
+export const dynamic = "force-dynamic"; // match the value used in ./dashboard/page
+
+export { metadata };
+export default DashboardPage;
+// export { default, dynamic, metadata } from "./dashboard/page";
