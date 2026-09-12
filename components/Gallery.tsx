540a0b1072d0564fbce11b7724fc8507653833cc
jad-png <j.belassiria3993@uca.ac.ma>
2026-09-12 19:33:07 +0100

    chore: commit all current changes

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -13,7 +13,7 @@ export default function Gallery() {
   const [expanded, setExpanded] = useState(false);
   return (
     <>
-      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-[1.25fr_.75fr_.95fr] lg:grid-rows-[280px_280px]">
+      <div className="grid min-w-0 grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-[1.25fr_.75fr_.95fr] lg:grid-rows-[280px_280px]">
       {images.map(([image, width, height], i) => (
         <a
           key={image}
